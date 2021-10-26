7a7f41a31286944d5b4ac58a4d9ec2ecb1932edd
ChabbehAymen <devaymen15.5.3@gmail.com>
2021-10-26 20:57:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/Preview.kt
@@ -0,0 +1,13 @@
+package com.example.diceroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DiceRollerTheme {
+
+    }
+}
